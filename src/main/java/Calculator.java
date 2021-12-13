140b90112cc5fccfc454764d022c7ab1b49f764e
KlausGrosser <klaus.grosser@deliveryhero.com>
2021-12-13 13:46:35 +0100

    Solutions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,15 +4,18 @@ public class Calculator {
 
     public int addition(int a, int b) {
         //TODO
+        return a + b;
     }
 
 
     public int subtract(int a, int b) {
         //TODO
+        return a - b;
     }
 
     public int multiply(int a, int b) {
         //TODO
+        return a * b;
     }
 
     public int division(int a, int b) {
@@ -21,6 +24,7 @@ public class Calculator {
             throw new IllegalArgumentException("Cannot division by zero");
         } else {
             //TODO
+            return a / b;
         }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,28 +10,35 @@ public class CalculatorTest {
     private static Calculator calculator;
 
     @BeforeAll
-    //TODO
+    static void init(){
+        calculator = new Calculator();
+    }
+
 
     @Test
     public void verifyAddition() {
         //TODO
+        assertEquals(25,calculator.addition(20,5));
     }
 
 
     @Test
     public void verifySubtraction() {
         //TODO
+        assertEquals(5,calculator.subtract(25,20));
     }
 
 
     @Test
     public void verifyMultiplication() {
         //TODO
+        assertEquals(500,calculator.multiply(25,20));
     }
 
     @Test
     public void verifyDivision() {
         //TODO
+        assertEquals(10,calculator.division(100,10));
     }
 
 
