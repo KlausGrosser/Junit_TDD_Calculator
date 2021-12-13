9a84b53f3291e9df1ed4066d960d81363c6ff105
KlausGrosser <klaus.grosser@deliveryhero.com>
2021-12-13 14:21:12 +0100

    Added verifyDivisionByZero() implementation

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -47,6 +47,6 @@ public class CalculatorTest {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
             calculator.division(5, 0);
         });
-        //TODO
+        assertEquals("Cannot divide by zero", exception.getMessage());
     }
 }
